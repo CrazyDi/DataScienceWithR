50bb046f70cafa57f254944820c1d22cb4e46a5d
CrazyDi <katuhis@yandex.ru>
2019-08-02 11:06:05 +0300

    Update DescriptionStatistics.R

--- DescriptionStatistics.R
+++ DescriptionStatistics.R
@@ -15,3 +15,27 @@ mean(df$mpg[df$cyl == 6 & df$vs == "V"])
 
 sd(df$hp[df$cyl != 3 & df$am == "Auto"])
 
+# ЗАДАЧА
+#  Рассчитайте среднее значение времени разгона (qsec) 
+# для автомобилей, число цилиндров (cyl) у которых 
+# не равняется 3 и показатель количества миль на галлон 
+# топлива (mpg) больше 20.
+result <- mean(df$qsec[df$cyl != 3 & df$mpg > 20])
+
+# Расчет статистик по группам
+?aggregate
+
+mean_hp_vs <- aggregate(x = df$hp, by = list(df$vs), FUN = mean)
+colnames(mean_hp_vs) <- c("VS", "Mean HP")
+
+aggregate(hp ~ vs, df, mean)
+
+aggregate(hp ~ vs + am, df, mean)
+aggregate(x = df$hp, by = list(df$vs, df$am), FUN = mean)
+
+# Несколько статистик по группам
+aggregate(x = df[, -c(8, 9)], by = list(df$am), FUN = median)
+aggregate(x = df[, c(1, 3)], by = list(df$am, df$vs), FUN = sd)
+
+aggregate(cbind(mpg, disp) ~ am + vs, df, sd)
+
